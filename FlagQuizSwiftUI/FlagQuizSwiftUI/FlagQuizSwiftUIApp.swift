399d7bc65ad2bc6432ed46b532bcaa2beb8d0f88
MartinLeeSJ <tjr2922@gmail.com>
2023-12-12 14:45:47 +0900

    [Feat] Common Setting

---
+++ FlagQuizSwiftUI/FlagQuizSwiftUI/FlagQuizSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagQuizSwiftUIApp.swift
+//  FlagQuizSwiftUI
+//
+//  Created by Martin on 12/12/23.
+//
+
+import SwiftUI
+
+@main
+struct FlagQuizSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
