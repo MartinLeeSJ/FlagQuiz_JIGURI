28a7da8625bddb2eee0e6493c988888a2487f523
MartinLeeSJ <tjr2922@gmail.com>
2023-12-30 18:06:38 +0900

    [Fix] 일부 CountryObject load 시 발생하는 오류 해결

--- FlagQuizSwiftUI/FlagQuizSwiftUI/Model/Country/FQCountryDetail.swift
+++ FlagQuizSwiftUI/FlagQuizSwiftUI/Model/Country/FQCountryDetail.swift
@@ -125,7 +125,8 @@ extension FQCountryDetail {
             longitude: longitude
         )
         
-        let areaWidth: Double = sqrt(Double(area * 1_000_000)) * 1.5
+        let adjustedArea: Double = abs(area * 1_000_000)
+        let areaWidth: Double = sqrt(adjustedArea) * 1.5
         
         return MKCoordinateRegion(center: center, latitudinalMeters: .init(floatLiteral: areaWidth), longitudinalMeters: .init(floatLiteral: areaWidth))
         
