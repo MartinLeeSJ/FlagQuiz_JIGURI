8f371e6b2048feb32b4ecc27452aa60f50e07e7d
MartinLeeSJ <tjr2922@gmail.com>
2024-02-09 15:23:38 +0900

    [Feat] Add FCM Settings

--- FlagQuizSwiftUI/FlagQuizSwiftUI/AppDelegate.swift
+++ FlagQuizSwiftUI/FlagQuizSwiftUI/AppDelegate.swift
@@ -6,9 +6,11 @@
 //
 
 import UIKit
+import UserNotifications
 
 import FirebaseCore
-import GoogleSignIn
+import FirebaseMessaging
+
 import GoogleMobileAds
 
 class AppDelegate: NSObject, UIApplicationDelegate {
@@ -16,21 +18,67 @@ class AppDelegate: NSObject, UIApplicationDelegate {
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
     ) -> Bool {
         FirebaseApp.configure()
+        googleAdMobService()
         
+        Task {
+            await notificationAuthorization()
+            application.registerForRemoteNotifications()
+        }
+        
+        
+        return true
+    }
+    
+    func application(
+        _ application: UIApplication,
+        didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
+    ) {
+        Messaging.messaging().apnsToken = deviceToken
+    }
+    
+    private func notificationAuthorization() async {
+        do {
+            try await  UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound])
+            UNUserNotificationCenter.current().delegate = self
+        } catch {
+            // TODO: Error Handling
+            print(error.localizedDescription)
+        }
+    }
+    
+    private func googleAdMobService() {
         DispatchQueue.main.async {
             let googleMobileAds =  GADMobileAds.sharedInstance()
             // Initialize the Google Mobile Ads SDK.
             googleMobileAds.start(completionHandler: nil)
             googleMobileAds.requestConfiguration.testDeviceIdentifiers = [ "d9f08d9cab2fd3e9547c5309d09a0d19" ]
         }
-        
-        return true
     }
     
-    func application(_ app: UIApplication,
-                     open url: URL,
-                     options: [UIApplication.OpenURLOptionsKey: Any] = [:]
-    ) -> Bool {
-        return GIDSignIn.sharedInstance.handle(url)
+    
+}
+
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    func userNotificationCenter(
+        _ center: UNUserNotificationCenter,
+        willPresent notification: UNNotification
+    ) async -> UNNotificationPresentationOptions {
+        
+        let userInfo = notification.request.content.userInfo
+        
+        Messaging.messaging().appDidReceiveMessage(userInfo)
+        
+        return [[.list, .banner, .sound]]
+    }
+    
+    func userNotificationCenter(
+        _ center: UNUserNotificationCenter,
+        didReceive response: UNNotificationResponse
+    ) async {
+        
+        let userInfo = response.notification.request.content.userInfo
+        
+        Messaging.messaging().appDidReceiveMessage(userInfo)
     }
 }
