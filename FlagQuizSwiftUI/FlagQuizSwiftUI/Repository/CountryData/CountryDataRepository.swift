98f10bc5bfb6651efec925fee0f2b4baa99bed3d
MartinLeeTyrell <martin.lee@tyrell.kr>
2026-01-10 13:28:47 +0900

    feature: complete country data repository impl

--- FlagQuizSwiftUI/FlagQuizSwiftUI/Repository/CountryData/CountryDataRepository.swift
+++ FlagQuizSwiftUI/FlagQuizSwiftUI/Repository/CountryData/CountryDataRepository.swift
@@ -24,8 +24,17 @@ public actor CountryDataRepositoryImpl: CountryDataRepository {
             guard let fileUrl = name.fileURL else { return }
             let dataFrame = try DataFrame(contentsOfCSVFile: fileUrl, columns: name.csvMetadata.columns, types: name.csvMetadata.types)
             switch name {
-            default: makeDatafromCountryRow(dataFrame)
+            case .base: makeDatafromCountryRow(dataFrame)
+            case .border: makeDataFromBorderRows(dataFrame)
+            case .capital: makeDataFromCapitalInfoRows(dataFrame)
+            case .capitalKr: makeDataFromCapitalKrRows(dataFrame)
+            case .continent: makeDataFromContinentRows(dataFrame)
+            case .currency: makeDataFromCurrencyRows(dataFrame)
+            case .flag: makeDataFromFlagRows(dataFrame)
+            case .map: makeDataFromMapRows(dataFrame)
+            case .timezone: makeDataFromTimeZoneRow(dataFrame)
             }
+            
         }
     return []
     }
@@ -57,14 +66,85 @@ public actor CountryDataRepositoryImpl: CountryDataRepository {
     }
     
     private func makeDataFromMapRows(_ dataFrame: DataFrame) {
-        
+        dataFrame.rows.forEach { row in
+            if let id = row["id"] as? Int,
+               let googleMapsUrl = row["google_maps_url"] as? String,
+               let openStreetMapsUrl = row["openstreetmaps_url"] as? String {
+                let countryData = data[id, default: .init(id: id)]
+                countryData.googleMapUrl = googleMapsUrl
+                countryData.openstreetMapUrl = openStreetMapsUrl
+            }
+        }
     }
     
+    private func makeDataFromFlagRows(_ dataFrame: DataFrame) {
+        dataFrame.rows.forEach { row in
+            if let id = row["id"] as? Int {
+                let countryData = data[id, default: .init(id: id)]
+                countryData.pngFlagUrl = row["png_url"] as? String
+                countryData.svgFlagUrl = row["svg_url"] as? String
+                countryData.flagAlt = row["alt"] as? String
+            }
+        }
+    }
+    
+    private func makeDataFromCurrencyRows(_ dataFrame: DataFrame) {
+        dataFrame.rows.forEach { row in
+            if let id = row["id"] as? Int {
+                let countryData = data[id, default: .init(id: id)]
+                countryData.currencyCode = row["currency_code"] as? String
+                countryData.currencyName = row["name"] as? String
+                countryData.currencySymbol = row["symbol"] as? String
+            }
+        }
+    }
+    
+    private func makeDataFromContinentRows(_ dataFrame: DataFrame) {
+        dataFrame.rows.forEach { row in
+            if let id = row["id"] as? Int,
+               let continent = row["continent"] as? String {
+                let countryData = data[id, default: .init(id: id)]
+                countryData.continents.append(continent)
+            }
+        }
+    }
+    
+    private func makeDataFromCapitalKrRows(_ dataFrame: DataFrame) {
+        dataFrame.rows.forEach { row in
+            if let id = row["id"] as? Int,
+               let capitalKr = row["capital_kr"] as? String {
+                let countryData = data[id, default: .init(id: id)]
+                countryData.capitalsKr.append(capitalKr)
+            }
+        }
+    }
+    
+    private func makeDataFromCapitalInfoRows(_ dataFrame: DataFrame) {
+        dataFrame.rows.forEach { row in
+            if let id = row["id"] as? Int {
+                let countryData = data[id, default: .init(id: id)]
+                countryData.capitalLat = row["lat"] as? Double
+                countryData.capitalLat = row["lng"] as? Double
+            }
+        }
+    }
+    
+    private func makeDataFromBorderRows(_ dataFrame: DataFrame) {
+        dataFrame.rows.forEach { row in
+            if let id = row["id"] as? Int,
+               let neighborId = row["neighbor_id"] as? Int {
+                let countryData = data[id, default: .init(id: id)]
+                countryData.neighborIds.append(neighborId)
+                
+            }
+        }
+    }
     
     
 }
 
 enum CountryCSVFileName: String, CaseIterable {
+    case base = ""
     case border
     case capital = "capital_info"
     case capitalKr = "capital_kr"
@@ -72,7 +152,6 @@ enum CountryCSVFileName: String, CaseIterable {
     case currency
     case flag
     case map
-    case base = ""
     case timezone
     
     var fileName: String {
